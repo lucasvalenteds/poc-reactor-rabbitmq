b50556bf6b4196ee6ba100960d961e300ecf796d
Lucas <lucasvalenteds@gmail.com>
2021-07-06 14:38:03 -0300

    Implemeting Chat mechanism
    * Using RabbitMQ Sender to publish messages
    * Using RabbitMQ Receiver to consume messages
    * Using Jackson to serialize and deserialize messages

---
+++ src/main/java/com/example/chat/Chat.java
@@ -0,0 +1,75 @@
+package com.example.chat;
+
+import com.example.queue.QueueEvent;
+import com.example.queue.QueueProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.AMQP;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.rabbitmq.AcknowledgableDelivery;
+import reactor.rabbitmq.OutboundMessage;
+import reactor.rabbitmq.Receiver;
+import reactor.rabbitmq.Sender;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public final class Chat {
+
+    private static final TypeReference<QueueEvent<Message>> QUEUE_EVENT_TYPE_REFERENCE = new TypeReference<>() {
+    };
+
+    private final Sender sender;
+    private final Receiver receiver;
+    private final ObjectMapper objectMapper;
+    private final QueueProperties queueProperties;
+
+    public Chat(Sender sender, Receiver receiver, ObjectMapper objectMapper, QueueProperties queueProperties) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.objectMapper = objectMapper;
+        this.queueProperties = queueProperties;
+    }
+
+    public Mono<UUID> send(String text) {
+        return Mono.just(text)
+            .map(Message::withContent)
+            .map(QueueEvent::createEvent)
+            .delayUntil(event ->
+                Mono.from(serializeEvent(event))
+                    .flatMapMany(messageBody -> sender.sendWithTypedPublishConfirms(Flux.just(messageBody)))
+            )
+            .map(event -> event.getPayload().getId());
+    }
+
+    public Flux<Message> read() {
+        return receiver.consumeManualAck(queueProperties.getQueue())
+            .doOnNext(AcknowledgableDelivery::ack)
+            .flatMap(delivery -> deserializeEvent(delivery.getBody()))
+            .map(QueueEvent::getPayload);
+    }
+
+    private <T> Mono<OutboundMessage> serializeEvent(QueueEvent<T> event) {
+        return Mono.fromCallable(() -> objectMapper.writeValueAsBytes(event))
+            .map(eventSerialized ->
+                new OutboundMessage(
+                    queueProperties.getExchange(),
+                    queueProperties.getRoutingKey(),
+                    new AMQP.BasicProperties.Builder()
+                        .appId(queueProperties.getAppId())
+                        .deliveryMode(queueProperties.getDeliveryMode())
+                        .contentType("application/json")
+                        .contentEncoding(StandardCharsets.UTF_8.displayName())
+                        .build(),
+                    eventSerialized)
+            )
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    private Mono<QueueEvent<Message>> deserializeEvent(byte[] event) {
+        return Mono.fromCallable(() -> objectMapper.readValue(event, QUEUE_EVENT_TYPE_REFERENCE))
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+}
---
+++ src/test/java/com/example/chat/ChatTest.java
@@ -0,0 +1,55 @@
+package com.example.chat;
+
+import com.example.queue.QueueProperties;
+import com.example.testing.IntegrationTestConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.context.ApplicationContext;
+import reactor.core.publisher.Flux;
+import reactor.rabbitmq.Receiver;
+import reactor.rabbitmq.Sender;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ChatTest extends IntegrationTestConfiguration {
+
+    public static final String MESSAGE_1 = "Have a great day, Mary!";
+    public static final String MESSAGE_2 = "Thanks John. Have a great day too!";
+
+    private Chat chat;
+
+    @BeforeEach
+    public void beforeEach(ApplicationContext context) {
+        this.chat = new Chat(
+            context.getBean(Sender.class),
+            context.getBean(Receiver.class),
+            context.getBean(ObjectMapper.class),
+            context.getBean(QueueProperties.class)
+        );
+    }
+
+    @RepeatedTest(10)
+    public void testSendingAndReadingMessages() {
+        var sending = Flux.just(MESSAGE_1, MESSAGE_2)
+            .flatMap(chat::send);
+
+        StepVerifier.create(sending)
+            .expectNextCount(2L)
+            .verifyComplete();
+
+        var reading = chat.read()
+            .take(2)
+            .collectList();
+
+        StepVerifier.create(reading)
+            .assertNext(messages ->
+                assertThat(messages)
+                    .hasSize(2)
+                    .extracting(Message::getContent)
+                    .containsOnly(MESSAGE_1, MESSAGE_2)
+            )
+            .verifyComplete();
+    }
+}
