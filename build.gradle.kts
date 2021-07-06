118fe5657cca7de52c67e1698fe273b364b2e30d
Lucas <lucasvalenteds@gmail.com>
2021-07-06 14:18:57 -0300

    Adding Spring, Reactor, RabbitMQ dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,17 @@ repositories {
 dependencies {
     implementation("org.apache.logging.log4j", "log4j-api", properties["version.log4j"].toString())
     implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
+    implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
+
+    implementation("org.springframework", "spring-core", properties["version.spring"].toString())
+    implementation("org.springframework", "spring-context", properties["version.spring"].toString())
+    testImplementation("org.springframework", "spring-test", properties["version.spring"].toString())
+
+    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
+    implementation("io.projectreactor.rabbitmq", "reactor-rabbitmq", properties["version.rabbitmq"].toString())
+    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
+
+    implementation("javax.annotation", "javax.annotation-api", properties["version.javax.annotation"].toString())
 
     testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
 }
