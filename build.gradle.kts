0f5857e720b071aa15e0003d4e9ee671f3d16460
Lucas <lucasvalenteds@gmail.com>
2021-07-06 14:08:39 -0300

    Adding minimal Gradle build script
    * Setting Java version
    * Configuring unit tests with JUnit 5

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+    }
+}
